1e76ad818db45862d076a44d2d61b83dff144f3c
=Oh Gyeong Taek <=>
2020-04-15 21:22:33 +0900

    3번문제 수정- 프로그래머스 버전

--- 3.js
+++ 3.js
@@ -1,50 +1,27 @@
-//https://tech.kakao.com/2017/09/27/kakao-blind-recruitment-round-1/
-
-'use strict'
-
-const tester = () => {
-    let vals = [
-        [ 3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"], 50 ],
-        [ 3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"], 21 ],
-        [ 2, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"], 60 ],
-        [ 5, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"], 52 ],
-        [ 2, ["Jeju", "Pangyo", "NewYork", "newyork"], 16 ],
-        [ 0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"], 25 ]
-    ]
-
-    vals.forEach(( val, idx ) => {
-        getActionTime(val) === val[2] 
-            ? console.log("True") 
-            : console.log("False");
-    });
-}
-
-/* val = [cacheSize, cities, result] */
-const getActionTime = (val) => {
-    if (val[0] === 0) return 5 * val[1].length;
-
-    let time = 0;
+//https://programmers.co.kr/learn/courses/30/lessons/17680
+function solution(cacheSize, cities) {
+    if (cacheSize === 0) return 5 * cities.length;
+    
+    let answer = 0;
     let cache = [];
+    let cityCnt = cities.length;
 
-    val[1].forEach(( city ) => {
-        city = city.toUpperCase();
-
+    cities.forEach((city, i) => {
+        city = city.toUpperCase(); 
         let idx = cache.indexOf(city);
-
+        
         if(idx > -1){
            cache.splice(idx, 1);
-           time += 1;
+           answer += 1;
         } else {
-            if ( cache.length >= val[0] ) {
+            if ( cache.length >= cacheSize ) {
                 cache.shift();
             }
-            time += 5;
+            answer += 5;
         }
         
         cache.push(city);
     });
-    
-    return time;
+     
+    return answer;
 }
-
-tester();
