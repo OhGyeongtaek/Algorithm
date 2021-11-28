61c5ff1c2fd104f98bef55a0aa1ba11ee4634ebe
OKT <rudxor97@naver.com>
2021-11-28 20:11:36 +0900

    필요없는 코드 제거

--- Monthly Code Challenge/code02.js
+++ Monthly Code Challenge/code02.js
@@ -1,47 +1,48 @@
 function solution(n) {
-    let answer = [];
-    const max = getMax(n);
-    const pyramid = [];
+  let answer = [];
+  const max = getMax(n);
+  const pyramid = [];
 
-    //피라미드 데이터 초기화
-    for (let i = 1; i <= n; i++) {
-        pyramid[i-1] = [];
-        for (let j = 0; j < i; j++) {
-            pyramid[i-1][j] = true;
-        }
+  //피라미드 데이터 초기화
+  for (let i = 1; i <= n; i++) {
+    pyramid[i - 1] = [];
+    for (let j = 0; j < i; j++) {
+      pyramid[i - 1][j] = true;
     }
-  
-    /*
+  }
+
+  /*
         x = x축 인덱스
         y = y축 인덱스
         v = value
     */
-    let x=0, y=0, v=1;
+  let x = 0,
+    y = 0,
+    v = 1;
 
-    pyramid[y][x] = v;
+  pyramid[y][x] = v;
 
-    while (v < max) {
-        while (pyramid[y+1] && pyramid[y+1][x] === true  && v < max) {
-            pyramid[++y][x] = ++v;
-        }
-
-        while (pyramid[y][x+1] === true && v < max) {
-            pyramid[y][++x] = ++v;
-        }
-
-        while (pyramid[y-1][x-1] === true && v < max) {
-            pyramid[--y][--x] = ++v;
-        }
+  while (v < max) {
+    console.log(y, x, v);
+    while (pyramid[y + 1] && pyramid[y + 1][x] === true) {
+      pyramid[++y][x] = ++v;
     }
 
-    pyramid.forEach((row) => {
-        answer = answer.concat(row);
-    });
-    return answer;
+    while (pyramid[y][x + 1] === true) {
+      pyramid[y][++x] = ++v;
+    }
+
+    while (pyramid[y - 1][x - 1] === true && v < max) {
+      pyramid[--y][--x] = ++v;
+    }
+  }
+
+  pyramid.forEach((row) => {
+    answer = answer.concat(row);
+  });
+  return answer;
 }
 
 function getMax(n) {
-    return n === 1 ? 1 : getMax(n-1) + n;
+  return n === 1 ? 1 : getMax(n - 1) + n;
 }
-
-console.log(solution(7));
