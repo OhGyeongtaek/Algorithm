b9bee32e1f43d270ac7a26655fe07ddb5715a61e
OKT <rudxor97@naver.com>
2021-12-07 01:14:27 +0900

    광고삽입 알고리즘 풀이 추가

---
+++ kakao/2021 KAKAO BLIND RECRUITMENT/05.js
@@ -0,0 +1,66 @@
+function solution(play_time, adv_time, logs) {
+  if (play_time === adv_time) {
+    return "00:00:00";
+  }
+
+  const play_second = toSeconds(play_time);
+  const adv_second = toSeconds(adv_time);
+
+  const acc = Array.from({ length: play_second + 1 }, () => 0);
+
+  logs.forEach((log) => {
+    const [start, end] = log.split("-");
+
+    acc[toSeconds(start)]++;
+    acc[toSeconds(end)]--;
+  });
+
+  let max = {
+    idx: 0,
+    value: 0,
+  };
+
+  for (let i = 1; i <= play_second; i++) acc[i] += acc[i - 1];
+
+  for (let i = 0; i <= adv_second; i++) max.value += acc[i];
+
+  let cur = max.value;
+
+  for (let i = adv_second + 1; i < play_second; i++) {
+    cur = cur - acc[i - adv_second] + acc[i];
+
+    if (cur > max.value) {
+      max = {
+        idx: i - adv_second + 1,
+        value: cur,
+      };
+    }
+  }
+
+  return toHHMMSS(max.idx);
+}
+
+const toSeconds = (time) => {
+  const values = time.split(":");
+
+  return values[0] * 60 * 60 + values[1] * 60 + parseInt(values[2]);
+};
+
+const toHHMMSS = (second) => {
+  const hh = Math.floor(second / 3600);
+  const mm = Math.floor((second % 3600) / 60);
+  const ss = second - hh * 3600 - mm * 60;
+
+  return `${hh.toString().padStart(2, "0")}:${mm
+    .toString()
+    .padStart(2, "0")}:${ss.toString().padStart(2, "0")}`;
+};
+
+console.log(
+  solution("99:59:59", "25:00:00", [
+    "69:59:59-89:59:59",
+    "01:00:00-21:00:00",
+    "79:59:59-99:59:59",
+    "11:00:00-31:00:00",
+  ])
+);
